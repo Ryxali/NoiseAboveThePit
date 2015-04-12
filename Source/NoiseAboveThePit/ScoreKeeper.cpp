b6b7c6bf4685779837ab73ba8d237a64b2188277
Koutyboy <victor.karlsson.github@gmail.com>
2015-04-12 13:01:16 +0200

    Added Hud stuff and Menu Stuff

---
+++ Source/NoiseAboveThePit/ScoreKeeper.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NoiseAboveThePit.h"
+#include "ScoreKeeper.h"
+
+uint64_t ScoreKeeper::Score = 0;
+
+ScoreKeeper::ScoreKeeper()
+{
+}
+
+ScoreKeeper::~ScoreKeeper()
+{
+}
